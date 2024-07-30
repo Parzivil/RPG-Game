d1ff6c44221af537013abf133a292bc87794dae1
Robin Nowlan <Robin.hexotech@gmail.com>
2024-07-30 15:29:46 +1200

    Created Project

---
+++ SoftwareConstuction_RPG-Game/src/main/java/group70/softwareconstuction_rpg/game/SoftwareConstuction_RPGGame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package group70.softwareconstuction_rpg.game;
+
+/**
+ *
+ * @author robin
+ */
+public class SoftwareConstuction_RPGGame {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
