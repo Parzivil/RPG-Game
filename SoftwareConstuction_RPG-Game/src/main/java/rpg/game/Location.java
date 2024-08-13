35b322df6d8aa70c10dd30cb1d9f8009aeb40410
Matthew-AT90 <mjbaker308@gmail.com>
2024-08-13 12:10:14 +1200

    Movement

--- SoftwareConstuction_RPG-Game/src/main/java/rpg/game/Location.java
+++ SoftwareConstuction_RPG-Game/src/main/java/rpg/game/Location.java
@@ -17,8 +17,8 @@ public class Location {
     
     enum Direction{
         NORTH,
-        SOUTH,
         EAST,
+        SOUTH,
         WEST
     }
 
@@ -47,6 +47,11 @@ public class Location {
     public int[] getCoordinates(){
         int[] coord = {xPosition, yPosition};
         return coord;
+    }
+    @Override
+    public String toString()
+    {
+        return (" x = "+xPosition+" y = "+yPosition+" ");
     }
     
 }
